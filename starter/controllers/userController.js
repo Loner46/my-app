4dab0c8d65b2a2b3c2f12fa118beff931c70a0a8
Makhmudov Alisher <m.alisher.1996@gmail.com>
2023-04-11 01:04:13 +0900

    Updated

--- starter/controllers/userController.js
+++ starter/controllers/userController.js
@@ -4,6 +4,7 @@ const User = require('../models/userModel');
 const catchAsync = require('../utils/catchAsync');
 const AppError = require('../utils/appError');
 const factory = require('./handlerFactory');
+const cloudinary = require('../utils/cloudinary');
 
 // const multerStorage = multer.diskStorage({
 //     destination: (req, file, cb) => {
@@ -17,85 +18,108 @@ const factory = require('./handlerFactory');
 const multerStorage = multer.memoryStorage();
 
 const multerFilter = (req, file, cb) => {
-    if (file.mimetype.startsWith('image')) {
-        cb(null, true);
-    } else {
-        cb(new AppError('uploaded file is not an image! Please upload only images.', 400), false)
-    }
-}
+  if (file.mimetype.startsWith('image')) {
+    cb(null, true);
+  } else {
+    cb(
+      new AppError(
+        'uploaded file is not an image! Please upload only images.',
+        400
+      ),
+      false
+    );
+  }
+};
 
 const upload = multer({
-    storage: multerStorage,
-    fileFilter: multerFilter
+  storage: multerStorage,
+  fileFilter: multerFilter,
 });
 
 exports.uploadUserPhoto = upload.single('photo');
 
-exports.resizeUserPhoto = catchAsync(async(req, res, next) => {
-    if (!req.file) return next();
-    req.file.filename = `user-${req.user.id}-${Date.now()}.jpeg`;
-    await sharp(req.file.buffer).resize(1000, 1000).toFormat('jpeg').jpeg({ quality: 90 }).toFile(`starter/public/img/users/${req.file.filename}`);
+exports.resizeUserPhoto = catchAsync(async (req, res, next) => {
+  if (!req.file) return next();
+  req.file.filename = `user-${req.user.id}-${Date.now()}.jpeg`;
+  await sharp(req.file.buffer)
+    .resize(1000, 1000)
+    .toFormat('jpeg')
+    .jpeg({ quality: 90 })
+    .toFile(`starter/public/img/users/${req.file.filename}`);
 
-    next();
+  next();
+});
+
+exports.uploadPhotoToCloudinary = catchAsync(async (req, res, next) => {
+  if (!req.file) return next();
+  req.file.filename = `user-${req.user.id}-${Date.now()}.jpeg`;
+  await cloudinary.uploader.upload(req.file.buffer);
+  next();
 });
 
 const filterObj = (obj, ...allowedFields) => {
-    const newObj = {};
-    Object.keys(obj).forEach(el => {
-        if (allowedFields.includes(el)) newObj[el] = obj[el];
-    })
-    return newObj;
-}
-
+  const newObj = {};
+  Object.keys(obj).forEach((el) => {
+    if (allowedFields.includes(el)) newObj[el] = obj[el];
+  });
+  return newObj;
+};
 
 exports.getMe = (req, res, next) => {
-    req.params.id = req.user.id;
-    next();
-}
+  req.params.id = req.user.id;
+  next();
+};
 
-exports.updateMe = catchAsync(async(req, res, next) => {
+exports.updateMe = catchAsync(async (req, res, next) => {
+  // 1. Create error if user POSTs password data
+  if (req.body.password || req.body.passwordConfirm) {
+    return next(
+      new AppError(
+        'This route is not for password updates. Please /updateMyPassword',
+        400
+      )
+    );
+  }
 
-    // 1. Create error if user POSTs password data
-    if (req.body.password || req.body.passwordConfirm) {
-        return next(new AppError('This route is not for password updates. Please /updateMyPassword', 400));
-    }
+  // 2. Filtered out unnecessary fields.
+  const filteredBody = filterObj(req.body, 'name', 'email');
+  if (req.file) {
+    filteredBody.photo = req.file.filename;
+  } else {
+    filteredBody.photo = 'default.jpg';
+  }
 
-    // 2. Filtered out unnecessary fields.
-    const filteredBody = filterObj(req.body, 'name', 'email');
-    if (req.file) {
-        filteredBody.photo = req.file.filename;
-    } else {
-        filteredBody.photo = 'default.jpg';
-    }
+  // 3. Update user document
+  const updatedUser = await User.findByIdAndUpdate(req.user.id, filteredBody, {
+    new: true,
+    runValidators: true,
+  });
 
-    // 3. Update user document
-    const updatedUser = await User.findByIdAndUpdate(req.user.id, filteredBody, { new: true, runValidators: true });
-
-    res.status(200).json({
-        status: 'success',
-        data: {
-            user: updatedUser
-        }
-    });
+  res.status(200).json({
+    status: 'success',
+    data: {
+      user: updatedUser,
+    },
+  });
 });
 
-exports.deleteMe = catchAsync(async(req, res, next) => {
-    await User.findByIdAndUpdate(req.user.id, { active: false });
+exports.deleteMe = catchAsync(async (req, res, next) => {
+  await User.findByIdAndUpdate(req.user.id, { active: false });
 
-    res.status(204).json({
-        status: 'success',
-        data: null,
-        message: 'Your account was deleted!'
-    });
-})
+  res.status(204).json({
+    status: 'success',
+    data: null,
+    message: 'Your account was deleted!',
+  });
+});
 
-exports.createUser = catchAsync(async(req, res) => {
-    res.status(500).json({
-        status: 'error',
-        data: null,
-        message: 'This route is not defined! Please access to /signup instead!'
-    });
-})
+exports.createUser = catchAsync(async (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    data: null,
+    message: 'This route is not defined! Please access to /signup instead!',
+  });
+});
 
 exports.getAllUsers = factory.getAll(User);
 exports.getUser = factory.getOne(User);
--- starter/dev-data/data/import-dev-data.js
+++ starter/dev-data/data/import-dev-data.js
@@ -4,6 +4,8 @@ const dotenv = require('dotenv');
 const Tour = require('../../models/tourModel');
 const User = require('../../models/userModel');
 const Review = require('../../models/reviewModel');
+const multer = require('multer');
+const cloudinary = require('../../utils/cloudinary');
 
 dotenv.config({ path: './starter/config.env' });
 
@@ -50,13 +52,23 @@ const importData = async () => {
   process.exit();
 };
 
+const uploadPhoto = async () => {
+  try {
+    await cloudinary.uploader.upload(
+      'C:UsersmalisOneDriveDesktopUdemyNode.jscomplete-node-bootcamp-mastercomplete-node-bootcamp-master\4-natoursstarterdev-dataimg\new-tour-4.jpg'
+    );
+  } catch (err) {
+    console.log(err);
+  }
+  process.exit();
+};
+
 if (process.argv[2] === '--import') {
   importData();
 } else if (process.argv[2] === '--delete') {
   deleteData();
-} else if (process.argv[2] === '--delete_import') {
-  deleteData();
-  importData();
+} else if (process.argv[2] === '--upload') {
+  uploadPhoto();
 }
 
 console.log(process.argv);
