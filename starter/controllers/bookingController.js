ee916eda61714bcc24a8f318a01ed8c15b783a40
Makhmudov Alisher <m.alisher.1996@gmail.com>
2023-05-03 19:47:08 +0900

    Updated

--- starter/controllers/bookingController.js
+++ starter/controllers/bookingController.js
@@ -97,7 +97,7 @@ exports.webhookCheckout = (req, res, next) => {
     const session = event.data.object;
     const tour = session.client_reference_id;
     // const user = (await User.findOne({ email: session.customer_email })).id;
-    const price = session.line_items[0].amount / 100;
+    // const price = session.line_items[0].amount / 100;
     console.log(`Tour:${tour}`);
   }
 
