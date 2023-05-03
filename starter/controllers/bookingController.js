ae0a61ad8d87cce2ced9140a7774b8b32b4d97b2
Makhmudov Alisher <m.alisher.1996@gmail.com>
2023-05-03 20:39:26 +0900

    Updated

--- starter/controllers/bookingController.js
+++ starter/controllers/bookingController.js
@@ -68,9 +68,12 @@ exports.checkIfBooked = catchAsync(async (req, res, next) => {
 const createBookingCheckout = async (session) => {
   const tour = session.client_reference_id;
   const user = (await User.findOne({ email: session.customer_email })).id;
-  const price = session.display_items[0].amount / 100;
+  // const price = session.display_items[0].amount / 100;
 
-  await Booking.create({ tour, user, price });
+  console.log(`Tour:${tour}`);
+  console.log(`User:${user}`);
+
+  // await Booking.create({ tour, user, price });
 };
 
 exports.webhookCheckout = (req, res, next) => {
